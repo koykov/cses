ce4dd20a2da14677d138f0c8b097dcae4a9674a6
Konstantin Voykov <koykov0@gmail.com>
2021-02-08 00:29:23 +0200

    "Permutations" issue.

---
+++ src/005.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file
+ * Permutations
+ * @see https://cses.fi/problemset/task/1070
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX_N = 1000000;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n = 0;
+    cin >> n;
+    if (n < 1 || n > MAX_N) {
+        return 1;
+    }
+
+    if (n == 1) {
+        cout << 1;
+        return 0;
+    }
+    
+    if (n < 4) {
+        cout << "NO SOLUTION";
+        return 0;
+    }
+
+    for (int i=2; i<=n; i+=2) {
+        cout << i << " ";
+    }
+    for (int i=1; i<=n; i+=2) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
